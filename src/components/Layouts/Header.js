a4da9b9d1628537c733f5edd58303b8fdc7184b5
Kiran Bawane <100428197+kiranbawane48@users.noreply.github.com>
2023-10-10 21:44:44 +0530

    Update Header.js

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.jpeg";
+import Logo from "../../assets/logo.png";
 import { Search } from "../Sections/Search";
 import { DropdownLoggedOut, DropdownLoggedIn } from "../index";
 import { useCart } from "../../context";
@@ -48,4 +48,3 @@ export const Header = () => {
     </header>
   )
 }
-
