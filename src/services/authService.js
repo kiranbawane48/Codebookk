cc088a404d46da39931a3289f376eb940ff36c9c
Kiran Bawane <100428197+kiranbawane48@users.noreply.github.com>
2023-10-10 22:09:11 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,44 +1,10 @@
-export async function login(authDetail) {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(authDetail),
-  };
-
-  try {
+export async function login(authDetail){
+    const requestOptions = {
+        method: "POST",
+        headers: {"content-Type": "application/json"},
+        body: JSON.stringify(authDetail)
+    }
     const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOptions);
-    
-    if (!response.ok) {
-      if (response.status === 400) {
-        throw new Error("This email is not registered. Please sign up.");
-      } else {
-        throw new Error(`HTTP error! Status: ${response.status}, Message: ${response.statusText}`);
-      }
-    }
-
-    const data = await response.json();
-
-    if (data.accessToken) {
-      sessionStorage.setItem("token", data.accessToken);
-      sessionStorage.setItem("cbid", data.user.id);
-    }
-
-    return data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-export async function register(authDetail) {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(authDetail),
-  };
-
-  try {
-    const response = await fetch(`${process.env.REACT_APP_HOST}/register`, requestOptions);
-
     if (!response.ok) {
       if (response.status === 400) {
         throw new Error("This email is already registered. Please log in.");
@@ -46,21 +12,41 @@ export async function register(authDetail) {
         throw new Error(`HTTP error! Status: ${response.status}, Message: ${response.statusText}`);
       }
     }
-
     const data = await response.json();
 
-    if (data.accessToken) {
-      sessionStorage.setItem("token", data.accessToken);
-      sessionStorage.setItem("cbid", data.user.id);
+    if(data.accessToken){
+        sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+        sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
     }
 
     return data;
-  } catch (error) {
-    throw error;
-  }
 }
 
-export function logout() {
-  sessionStorage.removeItem("token");
-  sessionStorage.removeItem("cbid");
+export async function register(authDetail){
+    const requestOptions = {
+        method: "POST",
+        headers: {"content-Type": "application/json"},
+        body: JSON.stringify(authDetail)
+    }  
+    const response = await fetch(`${process.env.REACT_APP_HOST}/register`, requestOptions);
+    if (!response.ok) {
+      if (response.status === 400) {
+        throw new Error("This email is not registered. Please sign up.");
+      } else {
+        throw new Error(`HTTP error! Status: ${response.status}, Message: ${response.statusText}`);
+      }
+    }
+    const data = await response.json();
+    
+    if(data.accessToken){
+        sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+        sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+    }
+
+    return data;
+}
+
+export function logout(){
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("cbid");
 }
